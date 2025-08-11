2b18de8e8c3aac404c272db449868bd6cb2f70b7
smartbiztoolkit <smartbiztoolkit@gmail.com>
2025-08-11 03:48:30 -0500

    test: configure jest and ignore deps

---
+++ ARIA_Master_Deploy_Enterprise 333/jest.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+};
